aa11011b794ba09af5849ef98a4800d54ab8a11f
dinhvandong <dongvan.ict3@gmail.com>
2024-09-04 11:26:41 +0700

    update backend
    update backend

---
+++ src/main/java/com/adda/picture_sale_backend/PictureSaleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adda.picture_sale_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureSaleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictureSaleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adda/picture_sale_backend/PictureSaleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adda.picture_sale_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureSaleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
